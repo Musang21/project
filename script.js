affce3017bef8d1866c965071eae24f0cd73db8a
Musang21 <sheenamagdayao524@gmail.com>
2025-02-12 22:14:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function baropen(x) {
+  x.classList.toggle("change");
+}
+
+
+function openmenu(){
+            if(openlink.style.maxHeight == "0px")
+            {
+                openlink.style.maxHeight = "300px";
+            }
+            else{
+                openlink.style.maxHeight = "0px";
+            }
+        }
